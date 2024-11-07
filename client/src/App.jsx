0495a004c61e2ad2f53725b9363d2763580c1162
weiqlu <weiqlu@vt.edu>
2024-11-07 12:46:17 -0500

    connected the frontend and backend, fetching the return and date

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import "./App.css";
-import Graph from "./components/Graph.jsx";
+import { useState, useEffect } from "react";
 
-function App() {
+function Graph() {
+  const [data, setData] = useState([]);
+
+  // fetches data only once when mounted
+  useEffect(() => {
+    fetch("http://localhost:5000/investure")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div>
-      <Graph />
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
 
-export default App;
+export default Graph;
